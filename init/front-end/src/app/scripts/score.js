488ebbc0226dd067f85875722f0b4678272486eb
ewarskotte <elea.warskotte@epfedu.fr>
2024-03-06 10:19:38 +0100

    Class

--- init/front-end/src/app/scripts/score.js
+++ init/front-end/src/app/scripts/score.js
@@ -1,22 +1,22 @@
 import template from "../views/score.html";
-  // TODO #class: use the ES6 class keyword
   /* class ScoreComponent constructor */
   import {parseUrl} from "./utils";
+import {Component} from "./component";
 
-export function ScoreComponent() {
-    // TODO #extends: call super(template)
-    var params = parseUrl();
-    this.template = template;
-    this.name = params.name;
-    this.size = parseInt(params.size);
-    this.time = parseInt(params.time);
+export class ScoreComponent extends Component{
+    constructor() {
+        super(template);
+        var params = parseUrl();
+        this.name = params.name;
+        this.size = parseInt(params.size);
+        this.time = parseInt(params.time);
+    }
+    con
+    init() {
+        document.getElementById("name").innerText = this.name;
+        document.getElementById("size").innerText = this.size;
+        document.getElementById("time").innerText = this.time;
+    }
+
   }
-
-  // TODO #class: turn function into a method of ScoreComponent
-  /* method ScoreComponent.init */
-  ScoreComponent.prototype.init = function init() {
-    document.getElementById("name").innerText = this.name;
-    document.getElementById("size").innerText = this.size;
-    document.getElementById("time").innerText = this.time;
-  };
 
